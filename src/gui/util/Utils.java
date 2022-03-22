adc1b4b27434e30f7f79e9565d389014cc77debb
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-03-22 17:34:15 +0000

    DepartmentForm (dialog) design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,20 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	
+	/**
+	 * Método para retornar o palco atual.
+	 * Primeiramente, ele pega o objeto onde aconteceu o evento através de sua origem que é de tipo Object, 
+	 * então, faz o downcasting para Node, em seguida, pega a cena atravês desse objeto Node e em seguida a janela através da cena
+	 * como a janela é uma superclasse do palco que é o que queremos, faz-se o downcasting para Stage.
+	 * @return
+	 */
+	public static Stage currentStage(ActionEvent event) {		
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();		
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module aula_58jdbcjavafxProject {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
